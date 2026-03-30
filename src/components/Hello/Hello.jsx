ae0c1896ff480e78ca75ce76b62f762c35c31344
AbulQasimbintariq <www.qasimbhalli6547@gmail.com>
2026-03-30 22:50:10 +0500

    first commit

---
+++ src/components/Hello/Hello.jsx
@@ -0,0 +1,8 @@
+export default function Hello() {
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold mb-4">Hello, World!</h1>
+            <p className="text-lg text-amber-500">Welcome to the React Router Web App.</p>
+        </div>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,37 @@
+ 
+import React from "react"
+import ReactDOM from "react-dom/client"
+// import App from "./App.jsx"
+import './App.css'
+import './index.css'
+import { createBrowserRouter, RouterProvider ,Route, createRoutesFromElements} from "react-router-dom"
+import Home from "./components/Home/Home.jsx"
+import Layout from "./Layout/Layout.jsx"
+import Contact from "./components/Contact/Contact.jsx"
+import Github, {githubInfoLoader} from "./components/Github/Github.jsx"
+import About from "./components/About/About.jsx"
+import User from "./components/User/User.jsx"
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />} >
+      <Route path="" element={<Home />} />
+      <Route path="About" element={<About />} />
+      <Route path="Contact" element={<Contact />} />
+      <Route
+        path="Github"
+        loader={githubInfoLoader}
+        element={<Github />} />
+      <Route path="User/:userid" element={<User />} />
+    </Route>
+  )
+)
+  
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
